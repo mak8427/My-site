1c426849d8a8aabc6c223288972a8717d9ed961f
Davide mattioli <83300800+mak8427@users.noreply.github.com>
2021-12-02 11:09:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+const rogi = document.querySelectorAll(".wipe-enter");
+
+observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`;
+    } else {
+      entry.target.style.animation = "none";
+    }
+  });
+});
+
+rogi.forEach((image) => {
+  observer.observe(image);
+});
+
+
+
+
