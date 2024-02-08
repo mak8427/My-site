3d3d44dcd1ce1855daedb4412c1800e1fa6d5983
mak8421 <davide.mattioli.2001@gmail.com>
2024-02-08 20:03:15 +0100

    dark button

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-document.getElementById('mode-switcher').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+document.addEventListener('DOMContentLoaded', (event) => {
+    function toggleMenu() {
+        const menu = document.querySelector(".menu-links");
+        const icon = document.querySelector(".hamburger-icon");
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+    }
+
+    let darkModeToggle = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    if (darkModeToggle) {
+        darkModeToggle.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+        });
+    } else {
+        console.log("Element with id 'dark-mode-toggle' not found");
+    }
 });
