5ccf0b04d055437af58732390f08ed170443214b
mak8421 <davide.mattioli.2001@gmail.com>
2024-02-12 19:40:43 +0100

    dark button

---
+++ finance/Portfolios/Portfolio2/Code_base.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+class Portfolio:
+    def __init__(self, code, cash, assets, weights):
+        self.code = code
+        self.assets= assets
+        self.cash = cash
+        self.weights = weights
+
+
+
+
+
+
