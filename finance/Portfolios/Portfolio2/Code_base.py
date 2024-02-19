57efdacfebe4e74e750a1352d6a4223ae8fb5291
mak8421 <davide.mattioli.2001@gmail.com>
2024-02-19 18:48:56 +0100

    dark button

--- finance/Portfolios/Portfolio2/Code_base.py
+++ finance/Portfolios/Portfolio2/Code_base.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
-
+import numpy.random
 class Portfolio:
-    def __init__(self, code, cash, assets, weights):
+    import numpy as np
+    def __init__(self, code, cash, assets,weights):
         self.code = code
         self.assets= assets
         self.cash = cash
-        self.weights = weights
+        self.weights= self.random_weight_initialise()
+    def random_weight_initialise(self):
+        weights=np.random.randint(0,10, size=(10))/10
+        return weights
 
 
-
-
-
-
+Portfolios=Portfolio(1,1,["s","S"], 3)
+print(Portfolios.weights)
