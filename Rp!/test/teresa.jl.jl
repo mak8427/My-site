c93415bb58bac475958f5ccc7f3d2fe9c8bb64fe
Davide <davide.mattioli.2001@gmail.com>
2024-06-17 19:29:37 +0200

    Update Journal teresa.jl.jl

--- Rp!/test/teresa.jl.jl
+++ Rp!/test/teresa.jl.jl
@@ -1,11 +1,38 @@
 ### A Pluto.jl notebook ###
-# v0.19.42
+# v0.19.9
 
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ ed0fcef9-6f15-4059-b649-8d50d282d325
+md" ![Elminsteer](https://cards.scryfall.io/normal/front/f/4/f4cd53a1-1544-4f4e-87ba-6f95dd5818d8.jpg)
+# Elmisteer 'il Mago pazzo'
+Si presenza come un signore sui 50 anni con una lunga barba bainca che lui sostiene sia frutto della sua saggezza. Questa saggezza l'ha spinto ad adorare solo la Magia come unico campo che abbia senso specializzarsi, in particolare, la Taumaturgia o in altre parole lo studio teorico della magia. Puo' risultare eccessivo a volte nella sua fissazione con tale argomento, poiche' praticamente ogni cosa deve avere a che fare con questa.
+
+Nonostante La sua vasta conoscenza in quest'ambito non ha praticamente esperienza nello studio pratico della Magia, e fa solo affidamento alloo studio teorico della magia per andare avanti
+"
+
 # ╔═╡ f32e1b00-2804-11ef-1c80-5ffb1716167f
 md"Diario dell'erborista"
 
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.10.3"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
 # ╔═╡ Cell order:
-# ╠═f32e1b00-2804-11ef-1c80-5ffb1716167f
+# ╟─ed0fcef9-6f15-4059-b649-8d50d282d325
+# ╟─f32e1b00-2804-11ef-1c80-5ffb1716167f
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
