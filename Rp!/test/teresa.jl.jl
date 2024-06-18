d24893d0e08e23a40e5accc20a081bff0feb0610
MimiUchiha <mimiuchiha1@gmail.com>
2024-06-18 17:57:48 +0200

    update

--- Rp!/test/teresa.jl.jl
+++ Rp!/test/teresa.jl.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.9
+# v0.19.42
 
 using Markdown
 using InteractiveUtils
@@ -10,6 +10,16 @@ md" ![Elminsteer](https://cards.scryfall.io/normal/front/f/4/f4cd53a1-1544-4f4e-
 Si presenza come un signore sui 50 anni con una lunga barba bainca che lui sostiene sia frutto della sua saggezza. Questa saggezza l'ha spinto ad adorare solo la Magia come unico campo che abbia senso specializzarsi, in particolare, la Taumaturgia o in altre parole lo studio teorico della magia. Puo' risultare eccessivo a volte nella sua fissazione con tale argomento, poiche' praticamente ogni cosa deve avere a che fare con questa.
 
 Nonostante La sua vasta conoscenza in quest'ambito non ha praticamente esperienza nello studio pratico della Magia, e fa solo affidamento alloo studio teorico della magia per andare avanti
+
+
+
+
+
+![image info](https://i.ibb.co/Vpydgc0/il-favo.png)
+![image info](https://i.ibb.co/rbn9skN/La-Perfide.png)
+
+
+![image info](https://i.ibb.co/2PsWsgd/Oleandro.png)
 "
 
 # ╔═╡ 3f9cb882-42e9-4e20-b465-41eed4837d22
@@ -35,7 +45,7 @@ project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 """
 
 # ╔═╡ Cell order:
-# ╟─ed0fcef9-6f15-4059-b649-8d50d282d325
+# ╠═ed0fcef9-6f15-4059-b649-8d50d282d325
 # ╠═3f9cb882-42e9-4e20-b465-41eed4837d22
 # ╟─f32e1b00-2804-11ef-1c80-5ffb1716167f
 # ╟─00000000-0000-0000-0000-000000000001
