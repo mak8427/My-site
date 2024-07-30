e2439f61d0b761d7da24bdecf0cd71eaee8e8d81
Davide <davide.mattioli.2001@gmail.com>
2024-07-30 14:04:14 +0200

    fix updater.sh

--- updater.sh
+++ updater.sh
@@ -10,21 +10,21 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Change to the directory of your repository
 cd "$SCRIPT_DIR" || exit 1
 
-#while true; do
-# Fetch all updates from the remote repository
-git fetch --all
+while true; do
+  # Fetch all updates from the remote repository
+  git fetch --all
 
-# Reset the local repository to match the remote repository
-git reset --hard origin/$BRANCH
+  # Reset the local repository to match the remote repository
+  git reset --hard origin/$BRANCH
 
-# Pull the latest changes from the remote repository
-git pull origin $BRANCH
+  # Pull the latest changes from the remote repository
+  git pull origin $BRANCH
 
-# Update submodules
-git submodule update --init --force --remote
+  # Update submodules
+  git submodule update --init --force --remote
 
-echo "Force pull and submodule update completed. Waiting for 10 seconds..."
+  echo "Force pull and submodule update completed. Waiting for 10 seconds..."
 
-# Wait for 10 seconds before repeating
-#  sleep 10
-#done
+  # Wait for 10 seconds before repeating
+  sleep 30
+done
