a6c30fb1472fb9f863b037da022cab806d56a0ed
Davide <davide.mattioli.2001@gmail.com>
2024-11-21 16:29:12 +0100

    Fixed light mode

--- Rp!/script.js
+++ Rp!/script.js
@@ -1,22 +1,22 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
+document.getElementById('theme-toggle').addEventListener('click', function () {
     const body = document.body;
     const button = this;
-    const icon = button.getElementsByTagName('i')[0]; // Correct way to access the icon
+    const icon = button.getElementsByTagName('i')[0];
     const isDarkMode = body.classList.contains('dark-mode');
 
-    // Toggle the class on the body
     if (isDarkMode) {
         body.classList.remove('dark-mode');
         body.classList.add('light-mode');
-        icon.className = 'fas fa-sun'; // Change icon to sun
-        button.innerHTML = '<i class="fas fa-sun"></i> Switch to Dark Mode'; // Update button text and keep the icon
+        icon.className = 'fas fa-sun';
+        button.innerHTML = '<i class="fas fa-sun"></i> Switch to Dark Mode';
     } else {
         body.classList.remove('light-mode');
         body.classList.add('dark-mode');
-        icon.className = 'fas fa-moon'; // Change icon to moon
-        button.innerHTML = '<i class="fas fa-moon"></i> Switch to Light Mode'; // Update button text and keep the icon
+        icon.className = 'fas fa-moon';
+        button.innerHTML = '<i class="fas fa-moon"></i> Switch to Light Mode';
     }
 });
+
 
 // Typing effect for the heading
 const heading = document.getElementById('animated-heading');
