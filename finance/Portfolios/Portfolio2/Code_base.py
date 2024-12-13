0c505a54630d61e41f46e9e37ddc6ddf7acfa2d0
Davide <davide.mattioli.2001@gmail.com>
2024-12-13 16:19:42 +0100

    Portfolio2.ipynb update

--- finance/Portfolios/Portfolio2/Code_base.py
+++ finance/Portfolios/Portfolio2/Code_base.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
 import numpy.random
-
-
+import yfinance as yf
 class Portfolio:
     import numpy as np
-    def __init__(self, code, cash, assets, weights):
+    def __init__(self, code, cash, assets_df, weights):
         self.code = code
-        self.assets = assets
+        self.asset_df = assets_df
         self.cash = cash
         self.weights = self.random_weight_initialise()
 
@@ -15,6 +14,8 @@ class Portfolio:
         weights = np.random.random(len(self.assets))
         weights /= weights.sum()
         return weights
+    def simulation:
+
 
 
 Portfolios = Portfolio(1, 1, ["s", "S"], 3)
